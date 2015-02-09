e67b08bfdc4f712016477efa87ae240108d67986
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-02-09 23:43:18 +0900

    参照先バージョン更新による更新

--- Project/VSHTC.Friendly.PinInterface.2.0/Properties/AssemblyInfo.cs
+++ Project/VSHTC.Friendly.PinInterface.2.0/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: InternalsVisibleTo("Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100617ded74188a8d46f711a2f0e7e73f5eb26e6f1481d6c1bcf8dc0b523588d8f6dad03bb8acdbf82e95515b1d4f5f396ebc80e9f1f559fc0ebe3004f742ef0cedc7d1ae8ab5390023eac2fd6fe933d729e9f87ce69bbb4ded3b9633718d7042086eeea72a9d4dabc2afb99a088571db865bc77921cb41dd1e3fdcce4c8f2bddc1")]
