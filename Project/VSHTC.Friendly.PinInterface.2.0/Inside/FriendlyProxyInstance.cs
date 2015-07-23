6a5fa1c06e60e532f347f5e320748fef69f5d70c
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-07-23 21:13:08 +0900

    Support for  System.__ComObject

--- Project/VSHTC.Friendly.PinInterface.2.0/Inside/FriendlyProxyInstance.cs
+++ Project/VSHTC.Friendly.PinInterface.2.0/Inside/FriendlyProxyInstance.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Reflection;
 using Codeer.Friendly;
+using System.Collections.Generic;
 
 namespace VSHTC.Friendly.PinInterface.Inside
 {
     class FriendlyProxyInstance<TInterface> : FriendlyProxy<TInterface>, IAppVarOwner, IModifyAsync
     {
+        bool _isComObject;
+
         public AppVar AppVar { get; private set; }
 
         public FriendlyProxyInstance(AppVar appVar)
             : base(appVar.App) 
         {
             AppVar = appVar;
+            _isComObject = !AppVar.IsNull && (string)AppVar["GetType"]()["FullName"]().Core == "System.__ComObject";
         }
 
         protected override AppVar Invoke(MethodInfo method, string name, object[] args, Async async, OperationTypeInfo typeInfo)
         {
-            return FriendlyInvokeSpec.GetFriendlyOperation(AppVar, name, async, typeInfo)(args);
+            if (_isComObject && typeInfo == null)
+            {
+                var argTypes = new List<string>();
+                foreach (var e in method.GetParameters())
+                {
+                    argTypes.Add(e.ParameterType.FullName);
+                }
+                typeInfo = new OperationTypeInfo(method.DeclaringType.FullName, argTypes.ToArray());
+                return FriendlyInvokeSpec.GetFriendlyOperation(AppVar, name, async, typeInfo)(args);
+            }
+            else
+            {
+                return FriendlyInvokeSpec.GetFriendlyOperation(AppVar, name, async, typeInfo)(args);
+            }
         }
 
         protected override string GetTargetTypeFullName()
