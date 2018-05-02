f1a553f555ad82652bd37f3c3cb60eb07e511cee
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2018-05-02 19:27:42 +0900

    As the dependency version upgraded, it follows it.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,4 +1,4 @@
 rd /s /q "../ReleaseBinary"
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../VSHTC.Friendly.PinInterface.sln" /rebuild Release
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "../VSHTC.Friendly.PinInterface.sln" /rebuild Release-English
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../VSHTC.Friendly.PinInterface.sln" /rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../VSHTC.Friendly.PinInterface.sln" /rebuild Release-English
 nuget pack Friendly.PinInterface.nuspec
